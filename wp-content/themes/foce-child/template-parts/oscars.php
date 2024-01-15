5318518267120829b5629ef6c006876ffcb75011
DevWp <aurelie.devwp@gmail.com>
2024-01-15 20:16:01 +0100

    ajout de la section oscar

---
+++ wp-content/themes/foce-child/template-parts/oscars.php
@@ -0,0 +1,4 @@
+<section id="oscars">
+    <h2 class="oscars-title">Fleurs d’oranger et chats errants est nominé aux Oscars Short Film Animated de 2022 !</h2>
+    <img src=<?php echo get_theme_file_uri("./assets/images/oscars-short-film-animated.png");?> alt="Nomination 2022 aux Oscars">
+</section>
